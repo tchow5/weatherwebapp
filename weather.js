bdc8eeb4fc07084dad16359234a592100e0ac820
tchow5 <timchow26@yahoo.com>
2015-04-18 21:54:25 -0400

    added api calling

--- weather.js
+++ weather.js
@@ -1,14 +1,43 @@
+
+
+function othername(){
+
+ var location = document.getElementById("userInput").value;
 	
+	ex(location);
+	return false;
+}
+// $(document).ready(function($) {
+//     $("button").click(function() {
+//             $.ajax({
+//                     url: "http://api.wunderground.com/api/d829236d086e9c28/geolookup/conditions/q/IA/" + location +".json",
+//                     dataType: "jsonp",
+//                     success: function(parsed_json) {
+//                         var location = parsed_json['location']['city'];
+//                         var temp_f = parsed_json['current_observation']['temp_f'];
+//                         alert("Current temperature in " + location + " is: " + temp_f);
+//                     });
+//             });
+   
 
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $(this).hide();
+//     });
+// });
 
-	jQuery(document).ready(function($) {
-		  $.ajax({
-			  url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
-			  dataType : "jsonp",
-			  success : function(parsed_json) {
-			  var location = parsed_json['location']['city'];
-			  var temp_f = parsed_json['current_observation']['temp_f'];
-			  alert("Current temperature in " + location + " is: " + temp_f);
-		  }
-	  });
+function ex(location){
+	$.get("http://api.wunderground.com/api/d829236d086e9c28/geolookup/conditions/q/IA/" + location  +".json",  function(data) {
+	        // do things with data
+	        console.log(data);
+	        
+	        dataoutput(data);
 	});
+}
+
+function dataoutput(data){
+	document.getElementById("dataoutput").innerHTML = JSON.stringify(data); 
+	console.log(data.current_observation);
+}
+
+
